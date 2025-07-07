7142e0d4896caa084167a4e9255fecfc5d631aec
Nom <nompalooza@gmail.com>
2025-07-07 16:02:14 -0700

    Route scaffold

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,3 +8,20 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 }
+
+func (app *application) confession(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "New Confession")
+}
+
+func (app *application) confessionPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post New Confession")
+}
+
+func (app *application) confessionViewId(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Fprintf(w, "View Confession %s", id)
+}
+
+func (app *application) confessionViewAll(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Viewing All Confessions")
+}
--- cmd/web/router.go
+++ cmd/web/router.go
@@ -6,6 +6,10 @@ func (app *application) router() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /confession", app.confession)
+	mux.HandleFunc("POST /confession", app.confessionPost)
+	mux.HandleFunc("GET /confession/{id}", app.confessionViewId)
+	mux.HandleFunc("GET /confession/all", app.confessionViewAll)
 
 	return mux
 }
