d6b7040f1f8c5b3471396538f03b227b4dcbf16d
Nom <nompalooza@gmail.com>
2025-07-08 14:37:53 -0700

    logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,31 +3,40 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-type application struct{}
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 	port := flag.String("port", "4242", "port to use")
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	err := godotenv.Load()
 	if err != nil {
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
 
-	app := &application{}
+	app := &application{
+		logger: logger,
+	}
 	srv := &http.Server{
-		Addr:    *port,
+		Addr:    ":" + *port,
 		Handler: app.router(),
 	}
 
 	fmt.Printf("Starting server on %s\n", *port)
 
 	err = srv.ListenAndServe()
+	logger.Error(err.Error())
 	os.Exit(1)
 }
